6e3fcd6afdd6e38b2a2a88da5bef7499db10c753
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-09 12:27:05 +0300

    user class

--- src/main/java/com/safari_store/ecommerce/users/models/User.java
+++ src/main/java/com/safari_store/ecommerce/users/models/User.java
@@ -1,16 +1,24 @@
 package com.safari_store.ecommerce.users;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import com.safari_store.ecommerce.users.models.Address;
+import com.safari_store.ecommerce.users.models.OTP;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
@@ -20,9 +28,64 @@ import java.util.List;
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @Column(unique = true)
+    @Email(message = "Please provide a valid email")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    private boolean isActive = false; // User starts inactive until email verification
+
+    @CreatedDate
+    private LocalDateTime dateJoined;
+
+    @LastModifiedDate
+    private LocalDateTime lastModified;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Column(unique = true)
+    private String nationalId;
+
+    private String profileImageUrl;
+
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Address> addresses;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OTP> otps;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collection.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
@@ -50,7 +113,7 @@ public class User implements UserDetails {
         return isActive;
     }
 
-    public enum Gender{
-        MALE, FEMALE, OTHER;
+    public enum Gender {
+        MALE, FEMALE, OTHER
     }
 }
--- src/main/java/com/safari_store/ecommerce/users/service/UserService.java
+++ src/main/java/com/safari_store/ecommerce/users/service/UserService.java
@@ -1,14 +1,16 @@
 package com.safari_store.ecommerce.users.service;
 
 import com.safari_store.ecommerce.users.User;
+import com.safari_store.ecommerce.users.dtos.request.UserProfileUpdateRequest;
+import com.safari_store.ecommerce.users.dtos.response.UserResponse;
 import com.safari_store.ecommerce.users.repository.AddressRepository;
 import com.safari_store.ecommerce.users.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +31,18 @@ public class UserService {
     }
 
     @Transactional(readOnly = true)
+    public UserResponse updateUserProfile(UserProfileUpdateRequest request){
+        User user = getCurrentUser();
+
+        if (request.getFirstName() != null){
+            request.setFirstName(request.getFirstName());
+        }
+        if (request.getLastName() != null){
+            request.setLastName(request.getLastName());
+        }
+        if (request.getEmail() != null && !request.getEmail().equals(user.getEmail())){
+
+        }
+    }
 
 }
