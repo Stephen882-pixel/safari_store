3c79bfc729dd90175e0b832779d705ef53207d30
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-10 12:56:02 +0300

    update profile controller

--- src/main/java/com/safari_store/ecommerce/users/controllers/UserController.java
+++ src/main/java/com/safari_store/ecommerce/users/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.safari_store.ecommerce.users.controllers;
 
 
+import com.safari_store.ecommerce.users.dtos.request.UserProfileUpdateRequest;
 import com.safari_store.ecommerce.users.dtos.response.ApiResponse;
 import com.safari_store.ecommerce.users.dtos.response.UserResponse;
 import com.safari_store.ecommerce.users.service.AddressService;
 import com.safari_store.ecommerce.users.service.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,24 @@ public class UserController {
                 "User data retrieved successfully",
                 userResponse
         ));
+    }
+
+    public ResponseEntity<ApiResponse<UserResponse>> updateProfile(
+            @Valid @RequestBody UserProfileUpdateRequest request
+            ){
+        log.info("Updating user profile");
+        try{
+            UserResponse updatedUser = userService.updateUserProfile(request);
+            return ResponseEntity.ok(ApiResponse.success(
+               "User profile update successfully",
+               updatedUser
+            ));
+        } catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(ApiResponse.error(
+               e.getMessage(),
+               null
+            ));
+        }
     }
 
 }
