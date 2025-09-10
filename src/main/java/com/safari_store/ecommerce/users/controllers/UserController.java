894fcfe6ec8bf7fdd84468f322b3568340da18ac
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-10 13:02:10 +0300

    get all users controller

--- src/main/java/com/safari_store/ecommerce/users/controllers/UserController.java
+++ src/main/java/com/safari_store/ecommerce/users/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.safari_store.ecommerce.users.controllers;
 
 import com.safari_store.ecommerce.users.dtos.request.UserProfileUpdateRequest;
 import com.safari_store.ecommerce.users.dtos.response.ApiResponse;
+import com.safari_store.ecommerce.users.dtos.response.PaginatedUsersResponse;
 import com.safari_store.ecommerce.users.dtos.response.UserResponse;
 import com.safari_store.ecommerce.users.service.AddressService;
 import com.safari_store.ecommerce.users.service.UserService;
@@ -10,9 +11,11 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -49,6 +52,26 @@ public class UserController {
             return ResponseEntity.badRequest().body(ApiResponse.error(
                e.getMessage(),
                null
+            ));
+        }
+    }
+
+    public ResponseEntity<ApiResponse<PaginatedUsersResponse>> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        log.info("Getting all users - page: {}, size: {}", page, size);
+        try{
+            PaginatedUsersResponse users = userService.getAllUsers(page, size);
+            return ResponseEntity.ok(ApiResponse.success(
+               "All users retrieved successfully",
+               users
+            ));
+        } catch (Exception e){
+            log.error("Error fetching users: ");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(
+               "Error fetching users: " + e.getMessage(),
+                    null
             ));
         }
     }
