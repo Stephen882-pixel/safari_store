8b8d7d10b50e41e55018259384c74c2eed2fd562
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-14 11:14:43 +0300

    promote controller

---
+++ src/main/java/com/safari_store/ecommerce/users/controllers/AdminController.java
@@ -0,0 +1,32 @@
+package com.safari_store.ecommerce.users.controllers;
+
+
+import com.safari_store.ecommerce.users.dtos.response.ApiResponse;
+import com.safari_store.ecommerce.users.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @PostMapping("/promote/{username}")
+    public ResponseEntity<ApiResponse<String>> promote(@PathVariable String username){
+        adminService.promoteToAdmin(username);
+        return ResponseEntity.ok(ApiResponse.success(
+           "User promoted to admin successfully",
+           "User " + username + " is now an ADMIN"
+        ));
+    }
+
+
+}
