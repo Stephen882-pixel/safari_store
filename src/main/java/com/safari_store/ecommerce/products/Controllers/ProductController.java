8db1cb805bf7e56737268614e199e80345ec7d9a
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-15 22:21:37 +0300

    getProductById  controller

--- src/main/java/com/safari_store/ecommerce/products/Controllers/ProductController.java
+++ src/main/java/com/safari_store/ecommerce/products/Controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class ProductController {
     public ResponseEntity<Page<ProductDTO>> getAllProducts(Pageable pageable){
         Page<ProductDTO> products = productService.getAllProducts(pageable);
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id){
+        ProductDTO productDTO = productService.getProductById(id);
+        return ResponseEntity.ok(productDTO);
     }
 }
