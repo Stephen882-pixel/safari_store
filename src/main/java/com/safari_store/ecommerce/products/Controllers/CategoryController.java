ec6a627a6224b312525e0abb437d41b706d9c924
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-15 22:12:41 +0300

    createCategory controller

--- src/main/java/com/safari_store/ecommerce/products/Controllers/CategoryController.java
+++ src/main/java/com/safari_store/ecommerce/products/Controllers/CategoryController.java
@@ -2,14 +2,15 @@ package com.safari_store.ecommerce.products.Controllers;
 
 import com.safari_store.ecommerce.products.DTOS.CategoryDTO;
 import com.safari_store.ecommerce.products.Service.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -31,5 +32,11 @@ public class CategoryController {
         return ResponseEntity.ok(categorry);
     }
 
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
+        CategoryDTO createdCategory = categoryService.createCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+    }
 
 }
