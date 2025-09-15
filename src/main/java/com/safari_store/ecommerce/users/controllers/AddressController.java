20a6e3cd0c8afb41c2bfd7dc4e9b95a4914282fc
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-15 15:44:39 +0300

    finalize testing addresses

--- src/main/java/com/safari_store/ecommerce/users/controllers/AddressController.java
+++ src/main/java/com/safari_store/ecommerce/users/controllers/AddressController.java
@@ -45,7 +45,7 @@ public class AddressController {
         try {
             AddressResponse addrress = addressService.createAddress(request);
             return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(
-               "Adderess create successfully",
+               "Adderess created successfully",
                addrress
             ));
         } catch (RuntimeException e) {
