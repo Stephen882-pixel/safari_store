91f68049073cfda1f7bfbeaa078e402e4ddccfc0
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-15 19:14:23 +0300

    convertToDTO method

--- src/main/java/com/safari_store/ecommerce/products/Service/CategoryService.java
+++ src/main/java/com/safari_store/ecommerce/products/Service/CategoryService.java
@@ -78,4 +78,18 @@ public class CategoryService {
         categoryRepository.save(category);
     }
 
+    private CategoryDTO convertToDTO(Category category){
+        CategoryDTO dto = new CategoryDTO();
+        dto.setId(category.getId());
+        dto.setName(category.getName());
+        dto.setDescription(category.getDescription());
+        dto.setImageUrl(category.getImageUrl());
+        dto.setActive(category.getActive());
+        dto.setCreatedAt(category.getCreatedAt());
+        dto.setUpdatedAt(category.getUpdatedAt());
+        return dto;
+    }
+
+
+
 }
