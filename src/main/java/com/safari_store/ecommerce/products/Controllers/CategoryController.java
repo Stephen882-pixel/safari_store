ba74b47522904a895e255d1db7b6ae13fd13e596
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-15 22:10:24 +0300

    getCategoryById controller

--- src/main/java/com/safari_store/ecommerce/products/Controllers/CategoryController.java
+++ src/main/java/com/safari_store/ecommerce/products/Controllers/CategoryController.java
@@ -4,12 +4,13 @@ import com.safari_store.ecommerce.products.DTOS.CategoryDTO;
 import com.safari_store.ecommerce.products.Service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -20,7 +21,15 @@ public class CategoryController {
 
     @GetMapping
     public ResponseEntity<Page<CategoryDTO>> getAllCategories(Pageable pageable){
-        Page<CategoryDTO> categories = categoryService.getCategoryById(id);
+        Page<CategoryDTO> categories = categoryService.getAllCategories(pageable);
         return ResponseEntity.ok(categories);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id){
+        CategoryDTO categorry= categoryService.getCategoryById(id);
+        return ResponseEntity.ok(categorry);
+    }
+
+
 }
