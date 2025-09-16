b83963208bdcf5b170ceccb271e52589034a8679
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-16 11:04:36 +0300

    Order status enum

--- src/main/java/com/safari_store/ecommerce/order/models/Order.java
+++ src/main/java/com/safari_store/ecommerce/order/models/Order.java
@@ -1,5 +1,25 @@
 package com.safari_store.ecommerce.order.models;
 
-@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+
+    private Long id;
+    private String orderNumber;
+    private Long userId;
+    private List<OrderItem> items = new ArrayList<>();
+    private OrderStatus status = OrderStatus.PENDING;
+
 }
---
+++ src/main/java/com/safari_store/ecommerce/order/models/OrderItem.java
@@ -0,0 +1,4 @@
+package com.safari_store.ecommerce.order.models;
+
+public class OrderItem {
+}
