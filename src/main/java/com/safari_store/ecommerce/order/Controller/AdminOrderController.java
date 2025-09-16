d16b132a907b913780d2f3a4d18580a69ec1b895
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-16 20:27:12 +0300

    get order by id controller

--- src/main/java/com/safari_store/ecommerce/order/Controller/AdminOrderController.java
+++ src/main/java/com/safari_store/ecommerce/order/Controller/AdminOrderController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin/orders")
@@ -32,5 +29,11 @@ public class AdminOrderController {
             orders = orderService.getAllOrders(pageable);
         }
         return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable Long orderId){
+        OrderDTO order = orderService.getOrderById(orderId);
+        return ResponseEntity.ok(order);
     }
 }
