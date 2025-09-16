f60ba1e6c6c9e90a46ed1df8792d01e2c9893839
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-16 14:49:55 +0300

    CancelOrderRequest

--- src/main/java/com/safari_store/ecommerce/order/DTOS/Request/CancelOrderRequest.java
+++ src/main/java/com/safari_store/ecommerce/order/DTOS/Request/CancelOrderRequest.java
@@ -1,6 +1,8 @@
 package com.safari_store.ecommerce.order.DTOS.Request;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CancelOrderRequest {
 
+    @NotBlank(message = "Cancellation reason is required")
+    @Size(max = 500, message = "Cancellation reason cannot exceed 500 characters")
+    private String reason;
 }
